286c58668b49de6c1a68f2ef24625317f51c0b10
AlejaMuGo <mamunozgo@unal.edu.co>
2023-10-16 16:07:04 -0500

    Update futbolista

--- futbolista.py
+++ futbolista.py
@@ -10,7 +10,7 @@ class Futbolista(Persona,Deportista):
         self._piernaHabil=piernaHabil
         Futbolista.listaFutbolistas.append(self)
     def __str__(self):
-        return  f"nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
+        return  f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
     @classmethod
     def getListaFutbolista(cls):
         return cls.listaFutbolistas
